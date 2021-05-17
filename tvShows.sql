574e7ad28e3c519354594bdc1992a0dc3794d41a
Asha Sanford <sanford.asha97@yahoo.com>
2021-05-17 00:39:53 -0400

    update

--- tvShows.sql
+++ tvShows.sql
@@ -57,36 +57,11 @@ SET tvProvider = 2 --Hulu
 WHERE id = 5; --Killing Eve
 
 
-SELECT * FROM authors;
 SELECT * FROM shows;
+SELECT * FROM showProvider;
 
--- show ALL books with authors
+-- show ALL tv shows from "shows" using the showProvider instead of tvProvider id number
 -- INNER JOIN will only return all matching values from both tables
 SELECT showName, providerName
 FROM shows
 INNER JOIN showProvider ON shows.tvProvider = showProvider.id;
-
--- show ALL books, even if we don't know the author
--- LEFT JOIN returns all of the values from the left table, and the matching ones from the right table
-SELECT title, firstName, lastName
-FROM books
-LEFT JOIN authors ON books.authorId = authors.id;
-
--- show ALL books, even if we don't know the author
--- RIGHT JOIN returns all of the values from the right table, and the matching ones from the left table
-SELECT title, firstName, lastName
-FROM books
-RIGHT JOIN authors ON books.authorId = authors.id;
-
-
--- CREATE TABLE authors(
---   id INTEGER(11) AUTO_INCREMENT NOT NULL,
---   firstName VARCHAR(100),
---   lastName VARCHAR(100),
---   PRIMARY KEY (id)
--- );
-
--- INSERT INTO authors (firstName, lastName) values ('Jane', 'Austen');
--- INSERT INTO authors (firstName, lastName) values ('Mark', 'Twain');
--- INSERT INTO authors (firstName, lastName) values ('Lewis', 'Carroll');
--- INSERT INTO authors (firstName, lastName) values ('Andre', 'Asselin');
